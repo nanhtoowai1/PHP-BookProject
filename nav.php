cc941d6147d3aa33c17a481e1f1307e9cdd4be6b
nanhtoowai1 <nanhtoowai1@gmail.com>
2021-02-04 17:07:27 +0630

    4/2021

--- nav.php
+++ nav.php
@@ -1,34 +1,42 @@
-<?php session_start()?>
+<?php session_start() ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container">
-      <a class="navbar-brand" href="#">Book Store</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
-          </li>
-          <li class="nav-item">
-            <?php
-            if (isset($_SESSION['user']) && isset($_SESSION['email'])) :
-                echo "Hello";
-            ?>
-
-            <?php else:?>
+  <div class="container">
+    <a class="navbar-brand" href="#">Book Store</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home
+            <span class="sr-only">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <?php
+          if (isset($_SESSION['user']) && isset($_SESSION['email'])) :
+          ?>
+            <div class="dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <?php echo $_SESSION['user'] ?>
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#"><?php echo $_SESSION['email'] ?></a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="logout.php">Log out</a>
+              </div>
+            </div>
+          <?php else : ?>
             <a class="nav-link" href="registerorlogin.php">Register or Login</a>
-            <?php endif?>
-          </li>
-        </ul>
-      </div>
+          <?php endif ?>
+        </li>
+      </ul>
     </div>
-  </nav>
+  </div>
+</nav>
--- register.php
+++ register.php
@@ -1,3 +1,27 @@
-<?php 
-$emailerr="";
+<?php
+$emailerr = "";
 require "admin/config/config.php";
+
+if (isset($_POST['btn_register'])) {
+	$name = htmlspecialchars($_POST['username']);
+	$email = htmlspecialchars($_POST['email']);
+	$password = htmlspecialchars($_POST['password']);
+	$hashpassword = md5($password);
+
+	$users = mysqli_query($conn, "SELECT * FROM users WHERE email='$email'");
+	$rowcount = mysqli_num_rows($users);
+	//echo $rowcount;
+	if ($rowcount > 0) {
+		$emailerr = "*Email is already exist!";
+	} else {
+		$sql = "INSERT INTO users(name,email,password) VALUES ('$name','$email','$hashpassword')";
+		$i = mysqli_query($conn, $sql);
+		if ($i) {
+			$_SESSION['user'] = $name;
+			$_SESSION['email'] = $email;
+			header("location:index.php");
+		} else {
+			echo $sql;
+		}
+	}
+}
